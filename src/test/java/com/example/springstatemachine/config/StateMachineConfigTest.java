06f0d0182047cd2936336fbe4ae86874e14bcf39
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-06-12 13:18:55 +0530

    unit test for state machine config

---
+++ src/test/java/com/example/springstatemachine/config/StateMachineConfigTest.java
@@ -0,0 +1,35 @@
+package com.example.springstatemachine.config;
+
+import com.example.springstatemachine.domain.PaymentEvent;
+import com.example.springstatemachine.domain.PaymentState;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+  @Autowired
+  StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+  @Test
+  void testNewStateMachine() {
+    StateMachine<PaymentState, PaymentEvent> stateMachine = factory.getStateMachine(UUID.randomUUID());
+
+    stateMachine.start();
+
+    System.out.println(stateMachine.getState());
+
+    stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+    System.out.println(stateMachine.getState());
+
+    stateMachine.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+    System.out.println(stateMachine.getState());
+
+  }
+}
